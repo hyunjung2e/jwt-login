baa2d8ad835864f5228ad99f86f1af9de66995bd
Hyunjung <hyunjung.kim@g2e.co.kr>
2024-08-29 10:44:38 +0900

    수정: :zap: cors 추가

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const jwt = require("jsonwebtoken");
 const bodyParser = require("body-parser");
 const fs = require("fs");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 const port = 5000;
 const secretKey = "secret_key"; // 실제 어플리케이션에서는 환경변수로 설정
 
+app.use(cors()); // 테스트를 위해 모든 출처에서 요청 허용
 app.use(bodyParser.json()); // 요청받는 형식이 JSON 일 때 자동 파싱해 req.body로 접근하게 해줌
 
 // 사용자 정보 데이터베이스
